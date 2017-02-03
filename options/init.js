64675645f8694c5f73c1d6e7ac20f12b29347eeb
okada <grandeur0714.ntc@gmail.com>
2017-02-03 09:44:09 +0000

    reborn option.html

--- options/init.js
+++ options/init.js
@@ -1,1 +1,1 @@
-//var chrome,$;window.onload=function(){for(var a=$(".setting"),c=[],b=0;b<a.length;b++)c.push(a[b].getAttribute("id"));chrome.storage.sync.get(c,function(b){for(var d=0;d<c.length;d++){var e=c[d],a=b[e];a?document.getElementById(e).value=a:document.getElementById(e).value=0}})};
+var chrome,$;window.onload=function(){for(var a=$(".setting"),c=[],b=0;b<a.length;b++)c.push(a[b].getAttribute("id"));chrome.storage.sync.get(c,function(b){for(var d=0;d<c.length;d++){var e=c[d],a=b[e];a?document.getElementById(e).value=a:document.getElementById(e).value=0}})};
--- options/save.js
+++ options/save.js
@@ -1,1 +1,1 @@
-//var chrome,$;$("#close_btn").click(function(){for(var b=$(".setting"),c=[],a=0;a<b.length;a++)c.push(b[a].getAttribute("id"));b={};for(a=0;a<c.length;a++)b[c[a]]=document.getElementById(c[a]).value;chrome.storage.sync.set(b,function(a){alert("\u4fdd\u5b58\u3057\u307e\u3057\u305f")})});
+var chrome,$;$("#close_btn").click(function(){for(var b=$(".setting"),c=[],a=0;a<b.length;a++)c.push(b[a].getAttribute("id"));b={};for(a=0;a<c.length;a++)b[c[a]]=document.getElementById(c[a]).value;chrome.storage.sync.set(b,function(a){alert("\u4fdd\u5b58\u3057\u307e\u3057\u305f")})});
